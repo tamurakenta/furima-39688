7b184fc05a3e6faf77950f88030d00e57a25f481
tamurakenta <139750737+tamurakenta@users.noreply.github.com>
2023-08-28 16:35:45 +0900

    モデルの単体テスト

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,22 +1,21 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :order
-  has_one_attached :item_image
+  has_one_attached :image
 
 
-  validates :item_image, presence: true
+  validates :image, presence: true
 
   validates :item_name, presence: true
   validates :item_info, presence: true
 
   validates :price, presence: true, format: { with: /\A[0-9]+\z/, message: "Half-width number" }, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_99_999, message: "Out of setting range" }
 
-
   with_options presence: true, numericality: { other_than: 1, message: 'Select'} do
   validates :item_category_id
-  validates :item_sales_status_id
+  validates :item_sales_status_id 
   validates :item_shipping_id
-  validates :item_prefecture_id
+  validates :item_prefecture_id 
   validates :item_scheduled_delivery_id
  end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,82 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+    @user = FactoryBot.build(:user)
+  end
+
+  describe '商品情報入力' do
+    context '新規登録できるとき' do
+    it '全ての値が正しく入力されていれば出品できること' do
+      item = FactoryBot.build(:item)
+      expect(@item).to be_valid
+    end
+  end
+
+
+  context '商品情報の入力がうまく行かない時' do
+    it 'imageが空だと出品できない' do
+      @item.image = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Image can't be blank")
+  end
+   it 'item_nameが空だと出品できない' do
+        @item.item_name = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item name can't be blank")
+   end
+   it 'item_infoが空だと出品できない' do
+    @item.item_info = nil
+    @item.valid?
+    expect(@item.errors.full_messages).to include("Item info can't be blank")
+  end
+  it 'categoryが未選択だと出品できない' do
+    @item.item_category_id = 1
+    @item.valid?
+    expect(@item.errors.full_messages).to include("Item category Select")
+  end
+  it 'item_conditionが未選択だと出品できない' do
+    @item.item_sales_status_id = 1
+    @item.valid?
+    expect(@item.errors.full_messages).to include("Item sales status Select")
+  end
+  it 'postageが未選択だと出品できない' do
+    @item.item_shipping_id = 1
+    @item.valid?
+    expect(@item.errors.full_messages).to include("Item shipping Select")
+  end
+  it 'prefectureが未選択だと出品できない' do
+    @item.item_prefecture_id = 1
+    @item.valid?
+    expect(@item.errors.full_messages).to include("Item prefecture Select")
+  end
+  it 'prepareが未選択だと出品できない' do
+    @item.item_scheduled_delivery_id = 1
+    @item.valid?
+    expect(@item.errors.full_messages).to include("Item scheduled delivery Select")
+  end
+  it 'priceが空だと出品できない' do
+    @item.price = nil
+    @item.valid?
+    expect(@item.errors.full_messages).to include("Price can't be blank")
+  end
+  it 'priceが全角数字だと出品できない' do
+    @item.price = "２０００"
+    @item.valid?
+    expect(@item.errors.full_messages).to include("Price Out of setting range")
+  end
+  it 'priceが¥300より少ない時は出品できない' do
+    @item.price = "299"
+    @item.valid?
+    expect(@item.errors.full_messages).to include("Price Out of setting range")
+  end
+  it 'priceが¥9999999より少ない時は出品できない' do
+    @item.price = "10_000_000"
+    @item.valid?
+    expect(@item.errors.full_messages).to include("Price Out of setting range")
+  end
+ end
 end
+end
+

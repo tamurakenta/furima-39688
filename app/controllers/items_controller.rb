7f70dee032bd543a36be4298413ab22c6db1be04
tamurakenta <139750737+tamurakenta@users.noreply.github.com>
2023-08-30 17:45:02 +0900

    コメントアウト方法の変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,11 +19,15 @@ class ItemsController < ApplicationController
     end
   end
 
+  <%
   =begin
+  %>
   def edit
     @item = Item.find(params[:id])
   end
+  <%
   =end
+  %>
  
   def show
     @item = Item.find(params[:id])

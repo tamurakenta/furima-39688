4bbbcf93d747ff46d6ec08fb93e2dccb7fbd7f38
tamurakenta <139750737+tamurakenta@users.noreply.github.com>
2023-08-31 21:21:12 +0900

    商品削除機能を実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:edit, :show, :update]
+  before_action :set_item, only: [:edit, :show, :update, :destroy]
     
   def index
     @items = Item.all.order(created_at: :desc)
@@ -17,6 +17,12 @@ class ItemsController < ApplicationController
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+   is item.user_id == current_user.id
+    @item.destroy
+    redirect_to root_path
   end
 
   def edit

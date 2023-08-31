df98a37d0b7add4e097a12d01272b5c7738518f8
tamurakenta <139750737+tamurakenta@users.noreply.github.com>
2023-08-31 12:15:00 +0900

    商品情報編集機能を実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,15 +21,31 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    if @item.user_id != current_user.id
+      redirect_to root_path     
+    end
   end
 
   def show
     @item = Item.find(params[:id])
   end
 
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+  end
+end
+
   private
 
   def item_params
     params.require(:item).permit(:item_image, :item_name, :item_info, :item_category_id, :item_sales_status_id, :item_shipping_id, :item_prefecture_id, :item_scheduled_delivery_id, :price).merge(user_id: current_user.id)
+  end
+
+  def user_params
+    params.require(:user).permit(:item_image, :item_name, :item_info, :item_category_id, :item_sales_status_id, :item_shipping_id, :item_prefecture_id, :item_scheduled_delivery_id, :price).merge(user_id: current_user.id)
   end
 end
